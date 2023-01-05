64f29cf1479cb8904759feefdc207a2887921aa4
NicolasGonzal <Nicolasgg1@hotmail.com>
2023-01-05 12:39:15 -0500

    Lista de productos añadida y maquetación avanzada

--- main.js
+++ main.js
@@ -4,22 +4,110 @@ const btnMobile = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
-
+const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 btnMobile.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
     aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
-function toggleMobileMenu(){
+function toggleMobileMenu() {
     aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
 }
-function toggleCarritoAside(){
+function toggleCarritoAside() {
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
-    aside.classList.toggle('inactive'); 
+    aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 12700,
+    imageURL: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Bicycle helmet',
+    price: 1200,
+    imageURL: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push({
+    name: 'Bicycle helmet',
+    price: 1600,
+    imageURL: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
+});
+productList.push({
+    name: 'Bicycle helmet',
+    price: 1500,
+    imageURL: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push({
+    name: 'Seat',
+    price: 300,
+    imageURL: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+});
+productList.push({
+    name: 'Tennis Montain Bike',
+    price: 2200,
+    imageURL: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+});
+productList.push({
+    name: 'Sunglasses',
+    price: 800,
+    imageURL: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+});
+productList.push({
+    name: 'Sunglasses',
+    price: 600,
+    imageURL: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
+});
+productList.push({
+    name: 'Bicycle seat bag',
+    price: 876,
+    imageURL: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+});
+
+//Es peferible hacer una función para posteriormente realizar un array que permita añadir la función a un boton u otro elemento.
+function addProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', product.imageURL);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+
+        const subDivProductInfo = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+
+        const productInfoFigure = document.createElement('figure');
+        const imgFigure = document.createElement('img');
+        imgFigure.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(imgFigure);
+
+        subDivProductInfo.append(productPrice, productName);
+
+        productInfo.append(subDivProductInfo, productInfoFigure);
+
+        productCard.append(img, productInfo);
+
+
+
+        cardsContainer.appendChild(productCard);
+
+    }
+}
+
+addProducts(productList);
